f3e482d36a5b2a13e69dd61a57e03d26ef423909
dtokic25 <danijel.tokic5@gmail.com>
2022-02-09 15:09:17 +0100

    Added empty class for sessions

---
+++ WebAdminApp/public_html/Classes/session.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Session 
+{
+    static function createSession()
+    {
+
+    }
+    static function createUser()
+    {
+
+    }
+    static function getUser()
+    {
+
+    }
+    static function deleteSession()
+    {
+        
+    }
+}
+
+?>
