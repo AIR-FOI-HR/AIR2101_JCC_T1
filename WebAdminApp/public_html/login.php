811c62687f79561d151569b0462933e5d5bc990e
dtokic25 <danijel.tokic5@gmail.com>
2022-02-09 15:31:05 +0100

    Added form for login

--- WebAdminApp/public_html/login.php
+++ WebAdminApp/public_html/login.php
@@ -5,6 +5,16 @@
 
 ?>
 
+<form method="post" action="login.php">
+    <label for="email">E-mail: </label>
+    <input id="email" name="email" type="text" placeholder="E-mail" autofocus/><br>
+
+    <label for="password">Password: </label>
+    <input id="password" name="password" type="password" placeholder="Password"/><br>
+
+    <input name="submit" type="submit" value="Log in"/>
+</form>
+
 <?php
     require "./footer.php";
 ?>
