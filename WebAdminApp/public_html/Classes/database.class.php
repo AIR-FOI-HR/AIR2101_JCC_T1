1ecf1ad2b8afd8e1e747fe5cd0ce294c56f728aa
dtokic25 <danijel.tokic5@gmail.com>
2022-02-09 14:55:26 +0100

    Added empty functions

---
+++ WebAdminApp/public_html/Classes/database.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database 
+{
+    function connectDB()
+    {
+
+    }
+    function closeDB()
+    {
+
+    }
+    function selectDB($query)
+    {
+
+    }
+    function updateDB($query)
+    {
+        
+    }
+}
+
+?>
