b840b073b250592cee2bb2c9816c71cc43bdb764
dtokic25 <danijel.tokic5@gmail.com>
2022-02-11 15:48:31 +0100

    Added redirection if user is not logged in on artworks page

---
+++ WebAdminApp/public_html/artwork.php
@@ -0,0 +1,14 @@
+<?php
+
+    $directory = getcwd();
+    require "./header.php";
+
+    if (empty($user))
+    {
+        header("Location: ./login.php");
+    }
+?>
+
+<?php
+    require "./footer.php";
+?>
