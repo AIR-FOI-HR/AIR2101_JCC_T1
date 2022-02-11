97c805a1ec68cb89af9f944cf5ba6fa020483a6b
dtokic25 <danijel.tokic5@gmail.com>
2022-02-11 16:24:43 +0100

    Added form inputs for new artwork page

--- WebAdminApp/public_html/newartwork.php
+++ WebAdminApp/public_html/newartwork.php
@@ -10,7 +10,22 @@
 ?>
 
 <form method="post" action="newartwork.php">
-    
+    <label for="name">Name: </label>
+    <input id="name" name="name" type="text" placeholder="Name" autofocus/><br>
+
+    <label for="author">Author: </label>
+    <input id="author" name="author" type="text" placeholder="Author"/><br>
+
+    <label for="arttype">Art Type: </label>
+    <select id="arttype" name="arttype">
+        
+    </select><br>
+
+    <label for="description">Description: </label>
+    <input id="description" name="description" type="text" placeholder="Description"/><br>
+
+    <label for="photo">Photo: </label>
+    <input id="photo" name="photo" type="text" placeholder="Photo"/><br>
 </form>
 
 <?php
