bed966d48a7dea74e2f6822bcef46e2e9caeeec3
dtokic25 <danijel.tokic5@gmail.com>
2022-02-11 16:02:08 +0100

    Added table print for all artwork data

--- WebAdminApp/public_html/artwork.php
+++ WebAdminApp/public_html/artwork.php
@@ -7,19 +7,39 @@
     {
         header("Location: ./login.php");
     }
+    $GLOBALS['museumID'] = $user[Session::MUSEUM];
 
-    $museumID = $user[Session::MUSEUM];
-    $connection = new Database();
-    $connection->connectDB();
-    $query = "SELECT * 
-        FROM `art` 
-        WHERE MuseumID=$museumID";
-    $result = $connection->selectDB($query);
-    while ($row = mysqli_fetch_array($result))
+    function printArtwork()
     {
-        
+        $connection = new Database();
+        $connection->connectDB();
+        $query = "SELECT * 
+            FROM `art` 
+            WHERE MuseumID=".$GLOBALS['museumID'];
+        $result = $connection->selectDB($query);
+        while ($row = mysqli_fetch_array($result))
+        {
+            echo "<tr>
+                <td>".$row['Name']."</td>
+                <td>".$row['Author']."</td>
+                <td>".$row['ArtTypeID']."</td>
+                <td>".$row['Description']."</td>
+                <td>".$row['Photo']."</td>
+            </tr>";
+        }
     }
 ?>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Author</th>
+        <th>Art Type</th>
+        <th>Description</th>
+        <th>Photo</th>
+    </tr>
+    <?php printArtwork()?>
+</table>
 
 <?php
     require "./footer.php";
