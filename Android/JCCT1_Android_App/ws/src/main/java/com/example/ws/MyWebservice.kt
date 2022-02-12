622d9a8076d65c568e54209f0e4a585241f10522
Marko-Zovko <mzovko@foi.hr>
2022-02-12 21:12:38 +0100

    Initializing WS

--- Android/JCCT1_Android_App/ws/src/main/java/com/example/ws/MyWebservice.kt
+++ Android/JCCT1_Android_App/ws/src/main/java/com/example/ws/MyWebservice.kt
@@ -1,4 +1,11 @@
 package com.example.ws
 
+import retrofit.http.Field
+import retrofit.http.FormUrlEncoded
+import retrofit.http.POST
+
 class MyWebservice {
+    @FormUrlEncoded
+    @POST("museums.php")
+    fun getAllMuseumsCaller(@Field(method) method: String) : Call<MyWebserviceResponse>
 }
