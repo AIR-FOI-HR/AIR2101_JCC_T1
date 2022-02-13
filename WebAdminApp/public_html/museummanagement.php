d0631a9b0fcd6de9e236dfc3b3b0a28831b8e4a3
dtokic25 <danijel.tokic5@gmail.com>
2022-02-13 18:30:28 +0100

    Added iot component print function

--- WebAdminApp/public_html/museummanagement.php
+++ WebAdminApp/public_html/museummanagement.php
@@ -26,10 +26,55 @@
         }
         $connection->closeDB();
     }
+    function printIoTComponents()
+    {
+        $connection = new Database();
+        $connection->connectDB();
+        $query = "SELECT * 
+            FROM `component` 
+            WHERE MuseumID=".$GLOBALS['museumID'];
+        $result = $connection->selectDB($query);
+        while ($row = mysqli_fetch_array($result))
+        {
+            $comptype = "";
+
+            $query = "SELECT *
+                FROM `componenttype` 
+                WHERE CompTypeID=".$row['CompTypeID'];
+            $result2 = $connection->selectDB($query);
+            $row2 = mysqli_fetch_array($result2);
+            if ($row2) $comptype=$row2['Name'];
+
+            echo "<tr>
+                <td>".$row['Name']."</td>
+                <td>".$comptype."</td>
+                <td>";
+            if ($row['TurnedOn'] == 1) echo "On";
+            else echo "Off";
+            echo "</td>
+                <td><a href=\"\">Change</a></td>
+                <td><a href=\"\">Update</a></td>
+                <td><a href=\"\">Delete</a></td>
+            </tr>";
+        }
+        $connection->closeDB();
+    }
 ?>
 
 Museum:<br>
 <?php printMuseumDetails(); ?>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Component Type</th>
+        <th>Turned On</th>
+        <th>Change State</th>
+        <th>Update</th>
+        <th>Delete</th>
+    </tr>
+    <?php printIoTComponents()?>
+</table>
 
 <?php
     require "./footer.php";
