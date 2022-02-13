3b83531860b0ba9d086b7aaffc96bcacd18b726e
dtokic25 <danijel.tokic5@gmail.com>
2022-02-13 18:23:56 +0100

    Added museum details print function

--- WebAdminApp/public_html/museummanagement.php
+++ WebAdminApp/public_html/museummanagement.php
@@ -7,8 +7,29 @@
     {
         header("Location: ./login.php");
     }
-    
+    $GLOBALS['museumID'] = $user[Session::MUSEUM];
+
+    function printMuseumDetails()
+    {
+        $connection = new Database();
+        $connection->connectDB();
+        $query = "SELECT * 
+            FROM `museum` 
+            WHERE MuseumID=".$GLOBALS['museumID'];
+        $result = $connection->selectDB($query);
+        if ($row = mysqli_fetch_array($result))
+        {
+            echo "<a>Name: ".$row['Name']."</a><br>
+            <a>Phone: ".$row['Phone']."</a><br>
+            <a>Email: ".$row['Email']."</a><br>
+            <a>Layout: ".$row['Layout']."</a><br>";
+        }
+        $connection->closeDB();
+    }
 ?>
+
+Museum:<br>
+<?php printMuseumDetails(); ?>
 
 <?php
     require "./footer.php";
