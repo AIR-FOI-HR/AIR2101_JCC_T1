6b364b9f04290c3e033d5b9f80cda0ce45f59b9a
Marko-Zovko <mzovko@foi.hr>
2022-02-13 15:30:52 +0100

    Changing Kotlin WS class to interface

--- Android/JCCT1_Android_App/ws/src/main/java/com/example/ws/MyWebservice.kt
+++ Android/JCCT1_Android_App/ws/src/main/java/com/example/ws/MyWebservice.kt
@@ -4,8 +4,10 @@ import retrofit.http.Field
 import retrofit.http.FormUrlEncoded
 import retrofit.http.POST
 
-class MyWebservice {
+interface MyWebservice {
+
     @FormUrlEncoded
     @POST("museums.php")
     fun getAllMuseumsCaller(@Field(method) method: String) : Call<MyWebserviceResponse>
+
 }
