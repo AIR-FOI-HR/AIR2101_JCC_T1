a06092025024a9fd653a724a7ae457870703e57c
Marko-Zovko <mzovko@foi.hr>
2022-02-13 15:19:25 +0100

    Creating a class Museum.kt in core module

---
+++ Android/JCCT1_Android_App/core/src/main/java/com/example/core/all/entities/Museum.kt
@@ -0,0 +1,11 @@
+package com.example.core.all.entities
+
+open class Museum (
+    open var museumId : Int? = null,
+    var name : String = "",
+    var phone : String = "",
+    var mail : String = "",
+    var address : String = "",
+    var layout : String = ""
+)
+
--- Android/JCCT1_Android_App/core/src/main/java/com/example/core/all/entities/Museums.kt
+++
@@ -1,4 +0,0 @@
-package com.example.core.all.entities
-
-class Museums {
-}
