ef7c5a4921f3f30c5132e1bd15de7d21cf14da05
Marko-Zovko <mzovko@foi.hr>
2022-02-15 18:41:13 +0100

    Renaming fragment and view model

--- Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/ui/gallery/ArtInfoFragment.kt
+++ Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/ui/gallery/ArtInfoFragment.kt
@@ -8,12 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.jcct1_android_app.R
 import com.example.jcct1_android_app.databinding.FragmentGalleryBinding
 
-class GalleryFragment : Fragment() {
+class ArtInfoFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var artInfoFragmentViewModel: ArtInfoFragmentViewModel
     private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,14 +24,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        artInfoFragmentViewModel =
+            ViewModelProvider(this).get(ArtInfoFragmentViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+        artInfoFragmentViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
