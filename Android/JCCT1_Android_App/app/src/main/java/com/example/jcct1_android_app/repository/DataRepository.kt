942eebb64d868c1798b108f7b87b353f7c8e4218
Marko-Zovko <mzovko@foi.hr>
2022-02-16 11:48:17 +0100

    changing core classes to fit data arrival

--- Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/repository/DataRepository.kt
+++ Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/repository/DataRepository.kt
@@ -1,9 +1,10 @@
 package com.example.jcct1_android_app.repository
 
+import com.example.core.all.entities.data.ArtworkDataSourceListener
 import com.example.core.all.entities.entities.Museum
 import com.example.ws.WsDataSource
 import com.example.core.all.entities.data.DataSource
-import com.example.core.all.entities.data.DataSourceListener
+import com.example.core.all.entities.data.MuseumDataSourceListener
 import com.example.core.all.entities.entities.Artwork
 
 
@@ -13,15 +14,12 @@ class DataRepository {
         var museumdataSource : DataSource
         museumdataSource = WsDataSource()
 
-        museumdataSource.loadData(
-            object : DataSourceListener {
+        museumdataSource.loadMuseumData(
+            object : MuseumDataSourceListener {
                 override fun onMuseumDataLoaded(museums: List<Museum>?) {
                     listenerMuseum.onMuseumDataLoaded(museums)
                 }
 
-                override fun onArtDataLoaded(artworks: List<Artwork>?) {
-                    TODO("Not yet implemented")
-                }
             }
         )
     }
@@ -31,14 +29,8 @@ class DataRepository {
         var artdataSource : DataSource
         artdataSource = WsDataSource()
 
-        artdataSource.loadData(
-            object : DataSourceListener {
-                override fun onMuseumDataLoaded(museums: List<Museum>?) {
-                    TODO("Not yet implemented")
-                    //Ovo ne bi trebalo biti ovako? U jednoj funkciji ne moraju biti svi overridei?
-                    //Problem sa coreom->data?
-                }
-
+        artdataSource.loadArtworkData(
+            object : ArtworkDataSourceListener {
                 override fun onArtDataLoaded(artworks: List<Artwork>?) {
                     listenerArtwork.onArtDataLoaded(artworks)
                 }
--- Android/JCCT1_Android_App/core/src/main/java/com/example/core/all/entities/data/ArtworkDataSourceListener.kt
+++ Android/JCCT1_Android_App/core/src/main/java/com/example/core/all/entities/data/ArtworkDataSourceListener.kt
@@ -1,9 +1,7 @@
 package com.example.core.all.entities.data
 
 import com.example.core.all.entities.entities.Artwork
-import com.example.core.all.entities.entities.Museum
 
-interface DataSourceListener {
-    fun onMuseumDataLoaded(museums: List<Museum>?)
+interface ArtworkDataSourceListener {
     fun onArtDataLoaded(artworks: List<Artwork>?)
 }
