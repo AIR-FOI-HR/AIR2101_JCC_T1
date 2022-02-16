8803fcb056d223a432fd36a8f770a6d252d45e7c
Marko-Zovko <mzovko@foi.hr>
2022-02-16 13:31:01 +0100

    Button more info now sends you to more info fragment (at this point still SlideshowFragment)

--- Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/recyclerview/DiscountViewHolder.kt
+++ Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/recyclerview/DiscountViewHolder.kt
@@ -1,21 +1,34 @@
 package com.example.jcct1_android_app.recyclerview
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.NonNull
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
 import com.bignerdranch.expandablerecyclerview.ChildViewHolder
 import com.example.core.all.entities.entities.Museum
 import com.example.jcct1_android_app.R
+import com.example.jcct1_android_app.ui.slideshow.SlideshowFragment
 
 class DiscountViewHolder: ChildViewHolder<Museum> {
     var museumDesc: TextView? = null
+    var moreInfoButton: Button? = null
 
     constructor(@NonNull itemView: View): super(itemView){
         museumDesc = itemView.findViewById(R.id.museum_desc)
+        moreInfoButton = itemView.findViewById(R.id.more_info)
 
     }
 
     public fun bindDataToView(discount: Museum){
         museumDesc?.text = discount.Layout
+        moreInfoButton?.setOnClickListener()
+        {
+            val controller = findNavController(itemView)
+            controller.navigate(R.id.nav_slideshow)
+
+        }
     }
 }
--- Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/ui/home/MuseumBrowsingFragment.kt
+++ Android/JCCT1_Android_App/app/src/main/java/com/example/jcct1_android_app/ui/home/MuseumBrowsingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -25,9 +26,12 @@ class MuseumBrowsingFragment : Fragment(), LoadMuseumDataListener {
     private lateinit var museumBrowsingViewModel: MuseumBrowsingViewModel
     private var _binding: FragmentHomeBinding? = null
 
+
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,7 +56,6 @@ class MuseumBrowsingFragment : Fragment(), LoadMuseumDataListener {
         viewReadyFlag = true
         DataRepository().loadMuseumData(this)
         tryToDisplayData()
-
     }
 
 
@@ -83,16 +86,10 @@ class MuseumBrowsingFragment : Fragment(), LoadMuseumDataListener {
         }
     }
 
- /*   fun loadDataToFragment()
-    {
-
-        print("blabla")
-        DataRepository().loadData(context, this)
-    }*/
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
     }
 }
