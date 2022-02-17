324fa47935cde312818a718ffe2db6fbe7a603f4
dtokic25 <danijel.tokic5@gmail.com>
2022-02-17 15:33:01 +0100

    Curators can now insert and update new events

--- WebAdminApp/public_html/newevent.php
+++ WebAdminApp/public_html/newevent.php
@@ -34,7 +34,32 @@
 
     if (isset($_POST['submit']))
     {
-       
+        $connection = new Database();
+        $connection->connectDB();
+        $query = "";
+        if (isset($_GET['id']))
+        {
+            $query = "UPDATE `event` 
+                SET `Name`='".$_POST['name']."', 
+                `Date`='".$_POST['date']."', 
+                `Time`='".$_POST['time']."', 
+                `MuseumID`='".$user[Session::MUSEUM]."', 
+                `UserID`='".$user[Session::ID]."'
+                WHERE EventID=".$_GET['id'];
+        }
+        else
+        {
+            $query = "INSERT INTO `event` 
+                VALUES (DEFAULT, 
+                '".$_POST['name']."', 
+                '".$_POST['date']."', 
+                '".$_POST['time']."', 
+                '".$user[Session::MUSEUM]."', 
+                '".$user[Session::ID]."')";
+        }
+        $result = $connection->updateDB($query);
+        $connection->closeDB();
+        header("Location: ./artwork.php");
     }
 ?>
 
@@ -43,10 +68,10 @@
     <input id="name" name="name" type="text" placeholder="Name" autofocus value="<?php if (!empty($_POST['name'])) echo $_POST['name'] ?>"/><br>
 
     <label for="date">Date: </label>
-    <input id="date" name="date" type="text" placeholder="Date" value="<?php if (!empty($_POST['date'])) echo $_POST['date'] ?>"/><br>
+    <input id="date" name="date" type="date" value="<?php if (!empty($_POST['date'])) echo $_POST['date'] ?>"/><br>
 
     <label for="time">Time: </label>
-    <input id="time" name="time" type="text" placeholder="Time" value="<?php if (!empty($_POST['time'])) echo $_POST['time'] ?>"/><br>
+    <input id="time" name="time" type="time" value="<?php if (!empty($_POST['time'])) echo $_POST['time'] ?>"/><br>
 
     <input name="submit" type="submit" value="Add artwork"/>
 </form>
