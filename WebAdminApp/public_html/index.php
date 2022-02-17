03b9e1ab858f15d1169beb27e7f0bb755ae963f7
dtokic25 <danijel.tokic5@gmail.com>
2022-02-17 17:16:49 +0100

    Added visits per artwork statistics

--- WebAdminApp/public_html/index.php
+++ WebAdminApp/public_html/index.php
@@ -3,6 +3,11 @@
     $directory = getcwd();
     require "./header.php";
 
+    if (empty($user))
+    {
+        header("Location: ./login.php");
+    }
+
     $GLOBALS['museumID'] = $user[Session::MUSEUM];
 
     function printVisits()
@@ -26,6 +31,28 @@
         }
         $connection->closeDB();
     }
+
+    function printArtworkVisits()
+    {
+        $connection = new Database();
+        $connection->connectDB();
+        $query = "SELECT a.Name, COUNT(v.Date) as Visits
+            FROM `visited` v
+            LEFT JOIN `art` a
+            ON a.ArtID=v.ArtID
+            WHERE a.MuseumID='".$GLOBALS['museumID']."'
+            GROUP BY v.ArtID
+            ORDER BY Visits DESC";
+        $result = $connection->selectDB($query);
+        while ($row = mysqli_fetch_array($result))
+        {
+            echo "<tr>
+                <td>".$row['Name']."</td>
+                <td>".$row['Visits']."</td>
+            </tr>";
+        }
+        $connection->closeDB();   
+    }
 ?>
 
 Visits by day:<br>
@@ -35,6 +62,15 @@ Visits by day:<br>
         <th>Visits</th>
     </tr>
     <?php printVisits()?>
+</table>
+<br>
+Visits by artwork:<br>
+<table>
+    <tr>
+        <th>Artwork</th>
+        <th>Visits</th>
+    </tr>
+    <?php printArtworkVisits()?>
 </table>
 
 <?php
