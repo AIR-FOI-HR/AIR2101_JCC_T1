2dd9f55571895e7d1f8342aceb3e4ca37a86e735
dtokic25 <danijel.tokic5@gmail.com>
2022-02-17 17:09:49 +0100

    Added print of visits by day

--- WebAdminApp/public_html/index.php
+++ WebAdminApp/public_html/index.php
@@ -2,7 +2,40 @@
 
     $directory = getcwd();
     require "./header.php";
+
+    $GLOBALS['museumID'] = $user[Session::MUSEUM];
+
+    function printVisits()
+    {
+        $connection = new Database();
+        $connection->connectDB();
+        $query = "SELECT Date, COUNT(v.Date) AS Visits
+            FROM `visited` v
+            LEFT JOIN `art` a
+            ON a.ArtID=v.ArtID
+            WHERE a.MuseumID='".$GLOBALS['museumID']."'
+            GROUP BY Date
+            ORDER BY Date DESC";
+        $result = $connection->selectDB($query);
+        while ($row = mysqli_fetch_array($result))
+        {
+            echo "<tr>
+                <td>".$row['Date']."</td>
+                <td>".$row['Visits']."</td>
+            </tr>";
+        }
+        $connection->closeDB();
+    }
 ?>
+
+Visits by day:<br>
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Visits</th>
+    </tr>
+    <?php printVisits()?>
+</table>
 
 <?php
     require "./footer.php";
